d72644209d227b38ed747221946b039b2407f1fd
Gentry Rolofson <0509479@my.scccd.edu>
2023-12-12 02:52:17 -0800

    improved installgo.sh
    This refactoring aims to enhance readability, reduce redundancy, and make the code more modular. Adjustments have been made to the structure and style to improve overall maintainability.

--- installgo.sh
+++ installgo.sh
@@ -1,29 +1,39 @@
-# TESTING IF UNAME EXISTS
-yell() { echo "$0: $*" >&2; }
-die() { yell "$*"; exit 111; }
-try() { "$@" || die "cannot $*"; }
-os=$(uname -s)
-# setup panic err control will close setup
-if [ -z $os ]; then die "Panic system does not have uname installed"; fi
-version="2.2.0"
-options=("Curl" "Wget")
-workspace=("Desktop" "Documents")
-PS3='Please enter your choice: '
-#get installed version of go
-localgo=$(go version | grep -Eo '\d+?(\.\d+)+?');
-go_version=$localgo
-#get current version of golang download url
-go_url=$(
-curl -s https://golang.org/dl/ 2>&1 |
-grep -Eoi '<a [^>]+>'               |
-grep -Eo 'href="[^\"]+"'            |
-grep -Eo 'https?://[^"]+'           |
-grep -i ${os}                       |
-sed -n 1p)
-# log version
-urlgo=$(echo $go_url | grep -Eo '\d+?(\.\d+)+?');
-local_dir=${PWD}
-echo "
+#!/bin/bash
+
+# Function for printing error messages
+yell() {
+  echo "$0: $*" >&2
+}
+
+# Function for displaying an error message and exiting
+die() {
+  yell "$*"
+  exit 111
+}
+
+# Function for executing a command and handling errors
+try() {
+  "$@" || die "cannot $*"
+}
+
+# Function to get the operating system
+get_os() {
+  uname -s
+}
+
+# Function to check if uname is installed
+check_uname() {
+  local os
+  os=$(get_os)
+  if [ -z "$os" ]; then
+    die "Panic system does not have uname installed"
+  fi
+}
+
+# Function to print the script banner
+print_banner() {
+  local version="2.2.0"
+  cat <<EOF
 ---------------------------------------------------------------------------------------------
 #  Automated Golang installer                                                               #
 #  ----------------------                                                                   #
@@ -34,187 +44,204 @@ echo "
 #  Github: @xDarkicex                                                                       #
 #                                                                                           #
 ---------------------------------------------------------------------------------------------
-"
+EOF
+}
 
-#DRY THAT CODE
-function workspace_setup() {
-  PS3="github.com/"
-  echo "enter github username"
-    while true; do
-      read -p ${PS3} username
-      break
-    done
+# Function to set up the workspace
+workspace_setup() {
+  local PS3="github.com/"
+  local username
+
+  echo "Enter GitHub username:"
+  read -p ${PS3} username
+
   echo $username
   cd ~/$workspace_dir &&
-  mkdir goworkspace &&
-  cd goworkspace &&
-  mkdir bin &&
-  mkdir src &&
-  mkdir pkg &&
-  cd src &&
-  mkdir github.com &&
-  cd github.com &&
-  mkdir $username &&
-  cd $username &&
-  mkdir example &&
-  cd example &&
-# Make sample File
-  echo "package main
+    mkdir -p goworkspace/{bin,src,pkg}/github.com/"$username"/example &&
+    # Make sample File
+    cat <<EOF >main.go
+package main
 
-import \"fmt\"
+import "fmt"
 
 func main() {
-fmt.Printf(\"hello, world\n\")
-}" > main.go &&
-#end sample files
+    fmt.Printf("hello, world\n")
+}
+EOF
+
+  # Rest of the workspace setup...
+
   Path='$PATH'
   cd ~ &&
-          echo "export GOROOT=\"/usr/local/go\"
+    echo "export GOROOT=\"/usr/local/go\"
 export GOPATH=\"$HOME/${workspace_dir}/goworkspace/src/${username}\"
 export PATH=\"$HOME/${workspace_dir}/goworkspace/bin:${Path}\"" >> .bash_profile &&
-          go env;
+    go env
   cd ${local_dir}
 }
-function workspace_local() {
+
+# Function to set up the local workspace
+workspace_local() {
   echo "-----------------------------------------------------------------------------------------"
-  echo "Where do you want your Go workspace path?
-Options: [Desktop||Documents]"
-    select opt in "${workspace[@]}"; do
-      case $opt in
-        "Desktop")
-          username=""
-          workspace_dir="Desktop"
-          before_workspace=${PWD}
-          workspace_setup
-            break;;
-        "Documents") echo "Documents stuff"
-          username=""
-          workspace_dir="Documents"
-          before_workspace=${PWD}
-          workspace_setup
-            break;;
-        esac
-      done
+  echo "Where do you want your Go workspace path? Options: [Desktop||Documents]"
 
-}
-
-function install() {
-  echo "install go"
-
-  cd ~ &&
-  current_dir=${PWD} &&
-  # echo $local_dir
-  echo "Do you want to use curl or wget?
-  Options: [1||2]"
-  select opt in "${options[@]}"; do
-      case $opt in
-          "Curl") cd ~ && curl -# ${go_url} > $current_dir/golang.pkg &&
-            sudo installer -pkg ${current_dir}/golang.pkg -target / &&
-            workspace_local
-            echo "-----------------------------------------------------------------------------------------"
-            break;;
-          "Wget") echo "Do you have wget installed?
-  Options: [1||2]"
-                select yn in "Yes" "No"; do
-                  case $yn in
-                    Yes )
-                    wget -P ${PWD} -O golang.pkg ${go_url} &&
-                    sudo installer -pkg ${PWD}/golang.pkg -target / &&
-                    workspace_local
-                    echo "-----------------------------------------------------------------------------------------"
-                    break;;
-                    No ) echo "homebrew required, do you have homebrew installed if unsure select no?
-  Options: [1||2]"
-                      select yn in "Yes" "No"; do
-                        case $yn in
-                          # This means that you have homebrew but not wget
-                          Yes ) brew install wget &&
-                          wget -P ${PWD} -O golang.pkg ${go_url} &&
-                          sudo installer -pkg ${PWD}/golang.pkg -target / &&
-                          workspace_local
-                          echo "-----------------------------------------------------------------------------------------"
-                          break;;
-                          # This means that you do not have homebrew!!!
-                          No )     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" &&
-                          brew install wget &&
-                          wget -P ${PWD} -O golang.pkg ${go_url} &&
-                          sudo installer -pkg ${PWD}/golang.pkg -target / &&
-                          workspace_local
-                          echo "-----------------------------------------------------------------------------------------"
-                          break;;
-                        esac
-                      done
-                      break;;
-                    esac
-                  done
-           break;;
-      esac
-  done
-}
-function update() {
-  echo "Updating to current golang version ${urlgo}"
-  echo "Do you want to use curl or wget?
-  Options: [1||2]"
-  select opt in "${options[@]}"; do
-      case $opt in
-          "Curl")
-          cd ~ &&
-          current_dir=${PWD}
-          curl -# ${go_url} > $current_dir/golang.pkg &&
-            sudo installer -pkg ${current_dir}/golang.pkg -target / &&
-            cd $local_dir
-            echo "-----------------------------------------------------------------------------------------"
-          break;;
-          "Wget")
-          cd ~ &&
-          wget -P ${PWD} -O golang.pkg ${go_url} &&
-          sudo installer -pkg ${PWD}/golang.pkg -target / &&
-          cd $local_dir
-          echo "-----------------------------------------------------------------------------------------"
-        break;;
-      esac
-    done
-}
-echo "Force update
-Warning Will Put you on Edge Versions of go.
-Options: [1||2]"
-force=("Yes" "No")
-    select opt in "${force[@]}"; do
-      case $opt in 
-        "Yes")
-        update
+  select opt in "${workspace[@]}"; do
+    case $opt in
+      "Desktop")
+        username=""
+        workspace_dir="Desktop"
+        before_workspace=${PWD}
+        workspace_setup
         break
-          ;;
-        "No")
-       break
-         ;;
+        ;;
+      "Documents")
+        echo "Documents stuff"
+        username=""
+        workspace_dir="Documents"
+        before_workspace=${PWD}
+        workspace_setup
+        break
+        ;;
     esac
   done
-go_exists=$(which go)
-if [ -z "$go_exists" ]
- then
-  install
+}
 
-elif [ "${localgo}" == "${urlgo}" ]
- then
-  echo "You are on  the most current version of golang ${localgo}"
+# Function to install Go
+install() {
+  echo "Install Go"
+  cd ~ &&
+    current_dir=${PWD} &&
+    echo "Do you want to use curl or wget? Options: [1||2]"
+  select opt in "${options[@]}"; do
+    case $opt in
+      "Curl")
+        cd ~ &&
+          curl -# ${go_url} > $current_dir/golang.pkg &&
+          try sudo installer -pkg $current_dir/golang.pkg -target / &&
+          workspace_local
+        break
+        ;;
+      "Wget")
+        echo "Do you have wget installed? Options: [1||2]"
+        select yn in "Yes" "No"; do
+          case $yn in
+            Yes)
+              wget -P ${PWD} -O golang.pkg ${go_url} &&
+                try sudo installer -pkg ${PWD}/golang.pkg -target / &&
+                workspace_local
+              break
+              ;;
+            No)
+              echo "Homebrew required. Do you have Homebrew installed? Options: [1||2]"
+              select yn in "Yes" "No"; do
+                case $yn in
+                  Yes)
+                    try brew install wget &&
+                      wget -P ${PWD} -O golang.pkg ${go_url} &&
+                      try sudo installer -pkg ${PWD}/golang.pkg -target / &&
+                      workspace_local
+                    break
+                    ;;
+                  No)
+                    try /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" &&
+                      try brew install wget &&
+                      wget -P ${PWD} -O golang.pkg ${go_url} &&
+                      try sudo installer -pkg ${PWD}/golang.pkg -target / &&
+                      workspace_local
+                    break
+                    ;;
+                esac
+              done
+              break
+              ;;
+          esac
+        done
+        break
+        ;;
+    esac
+  done
+}
+
+# Function to update Go
+update() {
+  echo "Updating to current golang version ${urlgo}"
+  echo "Do you want to use curl or wget? Options: [1||2]"
+  select opt in "${options[@]}"; do
+    case $opt in
+      "Curl")
+        cd ~ &&
+          current_dir=${PWD} &&
+          curl -# ${go_url} > $current_dir/golang.pkg &&
+          try sudo installer -pkg $current_dir/golang.pkg -target / &&
+          cd $local_dir
+        break
+        ;;
+      "Wget")
+        cd ~ &&
+          wget -P ${PWD} -O golang.pkg ${go_url} &&
+          try sudo installer -pkg ${PWD}/golang.pkg -target / &&
+          cd $local_dir
+        break
+        ;;
+    esac
+  done
+}
+
+# Main script
+
+check_uname
+
+# Set up panic err control will close setup
+version="2.2.0"
+options=("Curl" "Wget")
+workspace=("Desktop" "Documents")
+
+PS3='Please enter your choice: '
+
+# Get installed version of go
+localgo=$(go version | grep -Eo '\d+?(\.\d+)+?')
+go_version=$localgo
+
+# Get current version of golang download URL
+go_url=$(
+  curl -s https://golang.org/dl/ 2>&1 |
+    grep -Eoi '<a [^>]+>' |
+    grep -Eo 'href="[^\"]+"' |
+    grep -Eo 'https?://[^"]+' |
+    grep -i ${os} |
+    sed -n 1p
+)
+
+# Log version
+urlgo=$(echo $go_url | grep -Eo '\d+?(\.\d+)+?')
+local_dir=${PWD}
+
+print_banner
+
+echo "Force update. Warning: Will put you on Edge Versions of go. Options: [1||2]"
+force=("Yes" "No")
+select opt in "${force[@]}"; do
+  case $opt in
+    "Yes") update ;;
+    "No") break ;;
+  esac
+done
+
+go_exists=$(which go)
+if [ -z "$go_exists" ]; then
+  install
+elif [ "${localgo}" = "${urlgo}" ]; then
+  echo "You are on the most current version of Go ${localgo}"
   go env
 
-  echo "
+  cat <<EOF
           -------------------------------------------------------------
-          #         *******     congratulations      *******          #
+          #         *******     Congratulations      *******          #
           #                                                           #
-          #         goLang Version $go_version $os installed!              #  
+          #         GoLang Version $go_version installed!              #  
           -------------------------------------------------------------
-"
-
-
-
+EOF
   exit 0
 else
-
-## Everything else EXIT 0
- exit 1
+  exit 1
 fi
-
-
